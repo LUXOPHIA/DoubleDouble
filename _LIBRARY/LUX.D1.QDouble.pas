f5b4a0a735d1b1c2d419bcaf6b3deb09c305e3ec
luxidea <contact@luxidea.net>
2025-02-27 03:03:36 +0900

    TQDouble.MulQD / DivQD / Normalize4 メソッドを改良。

--- _LIBRARY/LUX.D1.QDouble.pas
+++ _LIBRARY/LUX.D1.QDouble.pas
@@ -52,7 +52,7 @@ implementation //###############################################################
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TDDouble
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TQDouble
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
 
@@ -80,35 +80,130 @@ var
 begin
      for I := 0 to 15 do Tmp[I] := 0.0;
 
-     Tmp[0] := X_.hi * Y_.hi;
+     Tmp[0]  := X_.hi  * Y_.hi;
+     Tmp[1]  := X_.hi  * Y_.me1;
+     Tmp[2]  := X_.hi  * Y_.me2;
+     Tmp[3]  := X_.hi  * Y_.lo;
+     Tmp[4]  := X_.me1 * Y_.hi;
+     Tmp[5]  := X_.me1 * Y_.me1;
+     Tmp[6]  := X_.me1 * Y_.me2;
+     Tmp[7]  := X_.me1 * Y_.lo;
+     Tmp[8]  := X_.me2 * Y_.hi;
+     Tmp[9]  := X_.me2 * Y_.me1;
+     Tmp[10] := X_.me2 * Y_.me2;
+     Tmp[11] := X_.me2 * Y_.lo;
+     Tmp[12] := X_.lo  * Y_.hi;
+     Tmp[13] := X_.lo  * Y_.me1;
+     Tmp[14] := X_.lo  * Y_.me2;
+     Tmp[15] := X_.lo  * Y_.lo;
 
      Result := Normalize4( Tmp );
 end;
 
+//------------------------------------------------------------------------------
+//  除算 4項 ÷ 4項 : Newton法で 1/Y_ を計算 → 乗算
+//------------------------------------------------------------------------------
 class function TQDouble.DivQD( const X_,Y_:TQDouble ) :TQDouble;
 var
-   XD, YD :Double;
-begin
-     XD := Double( X_ );
-     YD := Double( Y_ );
-
-     if ( YD = 0 ) then raise Exception.Create( 'TQDouble.DivQD: divide by zero' );
-
-     Result := TQDouble( XD / YD );
-end;
-
-class function TQDouble.Normalize4( const Arr_:array of Double ) :TQDouble;
-var
-   S :Double;
+   Z, Diff :TQDouble;
    I :Integer;
 begin
-     S := 0;
-     for I := 0 to High( Arr_ ) do S := S + Arr_[I];
+     // Y_==0 のチェック
+     if ( Double(Y_) = 0 ) then raise Exception.Create( 'TQDouble.DivQD: divide by zero' );
 
-     Result.hi  := S;
-     Result.me1 := 0;
-     Result.me2 := 0;
-     Result.lo  := 0;
+     // 1 / Y_ の初期近似 : double値で
+     Z := TQDouble( 1.0 / Double(Y_) );
+
+     // 2～3回の反復でQuadDouble精度に
+     for I := 1 to 2 do
+     begin
+          Diff := TQDouble(1.0) - ( Y_ * Z ); // (1 - Y_*Z)
+          Z := Z + ( Z * Diff );             // Z += Z*(1 - Y_*Z)
+     end;
+
+     // X_ * (1/Y_)
+     Result := X_ * Z;  // => MulQD => Normalize4
+end;
+
+//------------------------------------------------------------------------------
+//  Normalize4 : 与えられた配列 (最大8項や16項など) を 4項に圧縮
+//    1) 絶対値の大きい順にソート
+//    2) Left-to-right で 2Sum を繰り返し "expansion sum"
+//    3) 得られた拡張を最終的に4つの要素にまとめる
+//------------------------------------------------------------------------------
+class function TQDouble.Normalize4( const Arr_:array of Double ) :TQDouble;
+var
+   Buf :array of Double;
+   N :Integer;
+   i, j :Integer;
+   S, E :Double;
+   Comps :array[0..3] of Double;
+   LenC :Integer;
+begin
+     N := Length( Arr_ );
+     SetLength( Buf, N );
+
+     //--- 1) 配列をコピー ---
+     for i := 0 to N-1 do Buf[i] := Arr_[i];
+
+     //--- 2) 絶対値の大きい順にソート ---
+     for i := 0 to N-2 do
+     begin
+          for j := i+1 to N-1 do
+          begin
+               if Abs( Buf[j] ) > Abs( Buf[i] ) then
+               begin
+                    var tmp := Buf[i];
+                    Buf[i] := Buf[j];
+                    Buf[j] := tmp;
+               end;
+          end;
+     end;
+
+     //--- 3) Left-to-right で展開加算 (Shewchukの GrowExpansionに類似) ---
+     //     Comps[] に最大4項まで蓄積
+     Comps[0] := Buf[0];
+     Comps[1] := 0.0;
+     Comps[2] := 0.0;
+     Comps[3] := 0.0;
+     LenC := 1; // 現在何項あるか
+
+     for i := 1 to N-1 do
+     begin
+          var x := Buf[i];
+          // x を Comps[] に加算
+          // ここでは(Compsと x)を 2Sum しながら順に繰り込む
+          var newComps :array of Double;
+          SetLength( newComps, LenC+1 );
+
+          // Q に x を代入して、comps[i] と 2Sum
+          var Q := x;
+          for j := 0 to LenC-1 do
+          begin
+               // TQDouble互換: 2Sum( comps[j], Q, S, E )
+               S := Comps[j] + Q;
+               E := Q - ( S - Comps[j] );
+               Q := E;
+               newComps[j] := S;
+          end;
+          // 最後に Q を追加
+          newComps[LenC] := Q;
+
+          // newComps が (LenC+1)項になった
+          // ただし 4項を超えるなら切り詰める
+          if LenC < 4 then Inc( LenC ); // 項数増加(最大4)
+          // 再格納
+          for j := 0 to LenC-1 do Comps[j] := newComps[j];
+     end;
+
+     //--- 4) Comps に最大4項の展開が入ったはず
+     //        => hi, me1, me2, lo に順に格納
+     //        （絶対的に大きい順に並んでいるわけではないが、
+     //          nonoverlappingをできるだけ保持）
+     Result.hi  := Comps[0];
+     if LenC>1 then Result.me1 := Comps[1] else Result.me1 := 0;
+     if LenC>2 then Result.me2 := Comps[2] else Result.me2 := 0;
+     if LenC>3 then Result.lo  := Comps[3] else Result.lo  := 0;
 end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
@@ -175,7 +270,7 @@ end;
 function Abso( const X_:TQDouble ) :TQDouble;
 begin
      if X_.hi < 0 then Result := -X_
-                  else Result := X_;
+                  else Result := +X_;
 end;
 
 function Pow2( const X_:TQDouble ) :TQDouble;
@@ -196,7 +291,7 @@ begin
      end;
 
      G := TQDouble( Sqrt( D ) );
-     for I := 1 to 2 do G := G - ( Pow2( G ) - X_ ) / ( TQDouble( 2.0 ) * G );
+     for I := 1 to 2 do G := G - ( Pow2( G ) - X_ ) / ( 2 * G );
      Result := G;
 end;
 
@@ -208,7 +303,7 @@ var
 begin
      if Expon_ = 0 then
      begin
-          Result := TQDouble( 1.0 );
+          Result := 1;
           Exit;
      end;
 
@@ -224,7 +319,7 @@ begin
           E := E shr 1;
      end;
 
-     if NegP then R := TQDouble( 1.0 ) / R;
+     if NegP then R := 1 / R;
 
      Result := R;
 end;
@@ -248,7 +343,7 @@ begin
           Sum := Sum + Term;
      end;
 
-     Result := ( DA + Sum ) * TQDouble( 0.5 );
+     Result := ( DA + Sum ) * 0.5;
 end;
 
 function Ln( const X_:TQDouble ) :TQDouble;
